a34e4b112f0b06d4c77191ba6e425c7c50a0c15c
Sabrina fernandes <sabrina.soufer15@gmail.com>
2022-01-14 21:27:01 -0300

    Primeira alteração

--- script.js
+++ script.js
@@ -2,4 +2,21 @@ const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector ('#nome');
 const especie = document.querySelector ('#especie');
-const status = document.querySelector ('#status');
+const condicao = document.querySelector ('#status');
+
+
+pegarPersonagem = .() => {
+    return fetch(`https://rickandmortyapi.com/api/character/`, {
+        method: 'GET',
+        headers {
+            Accept: 'aplication/jeson',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.especie;
+    });
+}Teste greg
+botao.onclick = pegarPersonagem;
